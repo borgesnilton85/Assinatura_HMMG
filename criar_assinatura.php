b309acaef8767d510a6816ad8f3ee03c2a443aec
Nilton <borgesnilton85@gmail.com>
2018-10-26 12:28:39 -0300

    Update criar_assinatura.php

---
+++ criar_assinatura.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		 <meta charset="utf-8">
+		<title>Gerar Assinatura de Email</title>
+		<link href="css/bootstrap.min.css" rel="stylesheet">
+			<link href="css/estilo.css" rel="stylesheet">
+	</head>
+	<body>
+		<div class="container horizontal-center">
+			<div class="container horizontal-center">
+					<p class="text-center">
+						<h1 class="horizontal-center"> Assinatura Rede Mario Gatti</h1>
+					</p>
+				</div>
+		</div>
+		<div class="container">
+			<div class="row">
+				<div class="col align-self-start">
+    	    	</div>
+	   			<div class="col align-self-center">
+					<form action ="assinatura.php" target="frame">
+						<div class="col-sm-12">
+							<label for="nome">Nome : </label>
+							<input class ="form-control" type=text" name="nome" value="" placeholder="Nome">
+						</div>
+						<div class="col-sm-12">
+							<label for="funcao">Função</label>
+							<input class ="form-control" type=text" name="funcao" value="" placeholder="Função">
+						</div>
+						<div class="col-sm-12">
+							<label for="setor">Setor :</label>
+							<input class ="form-control"  type="text" name="setor" value="" placeholder="Setor">
+						</div>
+						<div class="col-sm-12">
+							<label for="telefone">Telefone: </label>
+							<input class ="form-control"  type="text" name="telefone" value="" placeholder="(19) 3772-5700">
+						</div>
+						<div class="col-sm-12">
+							<label for="email">E -mail: </label>
+							<input class ="form-control"  type="email" name="email" value="" placeholder="suporte.ti@hmmg.sp.gov.br">
+						</div>
+						<div class="col-sm-12">
+							<input   type="submit" name="enviar" value="Enviar">
+							<input   type="submit" name="codigo" value="Exibir Código">
+						</div>
+					</form>
+				</div>
+				<div class="col align-self-end">
+    			</div>
+			</div>
+			<div class="embed-responsive embed-responsive-16by9">
+				<iframe  class="embed-responsive-item" name="frame" src="assinatura.php" width="" height=""></iframe>
+			</div>
+		</div>
+	</body>
+</html>
