428607fc79e4302584188386b54257e0759b4d69
Nilton <borgesnilton85@gmail.com>
2018-10-26 12:29:03 -0300

    Update teste.php

---
+++ teste.php
@@ -0,0 +1,30 @@
+
+<?php
+$orig = "I'll \"walk\" the <b>dog</b> now";
+
+$a = htmlentities($orig);
+
+$b = html_entity_decode($a);
+
+echo $a; // I'll &quot;walk&quot; the &lt;b&gt;dog&lt;/b&gt; now
+
+echo $b; // I'll "walk" the <b>dog</b> now
+
+
+// For users prior to PHP 4.3.0 you may do this:
+function unhtmlentities($string) 
+{
+    // replace numeric entities
+    $string = preg_replace('~&#x([0-9a-f]+);~ei', 'chr(hexdec("\\1"))', $string);
+    $string = preg_replace('~&#([0-9]+);~e', 'chr("\\1")', $string);
+    // replace literal entities
+    $trans_tbl = get_html_translation_table(HTML_ENTITIES);
+    $trans_tbl = array_flip($trans_tbl);
+    return strtr($string, $trans_tbl);
+}
+
+$c = unhtmlentities($a);
+
+echo $c; // I'll "walk" the <b>dog</b> now
+
+?>
